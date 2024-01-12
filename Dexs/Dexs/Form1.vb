3cf0bb9cb2f06164314e261869cc385ae6ad1f9c
cade <crmcd131@learner.dvusd.org>
2024-01-12 12:40:48 -0700

    records

--- Dexs/Dexs/Form1.vb
+++ Dexs/Dexs/Form1.vb
@@ -1,4 +1,7 @@
-﻿Public Class Form1
+﻿Imports System.IO
+
+Public Class Form1
+    Dim records(50) As String
     Private Sub NewToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NewToolStripMenuItem.Click
         field1.Text = ""
         field2.Text = ""
@@ -30,4 +33,15 @@
     Private Sub OpenFileDialog1_FileOk(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles OpenFileDialog1.FileOk
         PictureBox1.Load(OpenFileDialog1.FileName)
     End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If IO.File.Exists("data.txt") Then
+
+            Dim inFile As New IO.StreamReader("data.txt")
+            records(0) = inFile.ReadLine
+            records(1) = inFile.ReadLine
+            inFile.Close()
+        End If
+    End Sub
+
 End Class
